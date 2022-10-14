75b81cab7cdac99261f9bd36220980c27ae50a8f
LordArchFist <degardj@protonmail.com>
2022-10-13 18:35:16 -0700

    update react router to host images

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -20,19 +20,18 @@ import Navbar from './Navbar'
         <a href="https://github.com/EyesWithoutFace/Fortune-Teller"><li>Fortune-Teller</li></a>
         <a href="https://github.com/EyesWithoutFace/PySimpleGui-Calculator"><li>Python Calculator</li></a>
         <a href="https://github.com/EyesWithoutFace/Snake-Game-Python"><li>Snake Game (Python)</li></a>
-        <a href="./dima/dima.html"><li>Dima the Cat</li></a>
     </ul>
 </div>
 </article>
 </div>
 <h2>Skills</h2>
 <div id="skills"></div>
-<img src="html5.png" alt="HTML" id="html5"/>
-<img src="css3.png" alt="CSS" id="css3"/>
+<img src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/html5.png?raw=true" alt="HTML" id="html5"/>
+<img src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/css3.png?raw=true" alt="CSS" id="css3"/>
 <img src="javascript.png" alt="Javascript" id="javascript-img"/>
-<img src="cli.png" alt="Command Line(Linux)" id="cli-img"/>
-<img src="git.png" alt="Git" id="git-img"/>
-<img src="python.png" alt="Python" id="python-img"/>
+<img src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/cli.png?raw=true" alt="Command Line(Linux)" id="cli-img"/>
+<img src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/git.png?raw=true" alt="Git" id="git-img"/>
+<img src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/python.png?raw=true" alt="Python" id="python-img"/>
 </main>
   );
 }
--- react-app/src/dima.js
+++ react-app/src/dima.js
@@ -11,11 +11,11 @@ const Dima = () => {
             and corner was explored.
         </p>
         <img className='kitten-dima'src="kitten-dima.jpg" alt="kitten dima"/>
-        <img className="peekaboo-dima" src="../public/peekaboo-dima.jpg" alt="dima playing peekaboo"/>
-        <img className="utility-dima" src="dima-utility.jpg" alt="dima on top of washer"/>
-        <img className="dima-dresser" src="dima-dresser.jpg" alt="dima under the dresser"/>
-        <img className="dima-cat-tree" src="dima-cat-tree.jpg" alt="dima on cat tree"/>
-        <img className="dima-litter" src="dima-litter.jpg" alt="dima in the litterbox"/>
+        <img className="peekaboo-dima" src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/peekaboo-dima.jpg?raw=true" alt="dima playing peekaboo"/>
+        <img className="utility-dima" src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/dima-utility.jpg?raw=true" alt="dima on top of washer"/>
+        <img className="dima-dresser" src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/dima-dresser.jpg?raw=true" alt="dima under the dresser"/>
+        <img className="dima-cat-tree" src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/dima-cat-tree.jpg?raw=true" alt="dima on cat tree"/>
+        <img className="dima-litter" src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/dima-litter.jpg?raw=true" alt="dima in the litterbox"/>
     </div>
         <div className="danger-block">
             <h2 className="journey">Her unfortunate journey</h2>
@@ -26,9 +26,9 @@ const Dima = () => {
                   amputation was an option if I didn't pay for surgery. I paid for the 
                 surgery and made sure she got plenty of rest.
             </p>
-            <img className="dima-x-ray" src="dima-x-ray.jpg" alt="x-ray of dimas arm"/>
-            <img className="dima-cone" src="dima-cone.jpg" alt="dima in a cone"/>
-            <img className="dima-rest" src="dima-rest.jpg" alt="dima resting on a pillow"/>
+            <img className="dima-x-ray" src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/dima-x-ray.jpg?raw=true" alt="x-ray of dimas arm"/>
+            <img className="dima-cone" src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/dima-cone.jpg?raw=true" alt="dima in a cone"/>
+            <img className="dima-rest" src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/dima-rest.jpg?raw=true" alt="dima resting on a pillow"/>
         </div>
         <div className="current-dima">
             <h2 className="current-dima-h2">Current Dima</h2>
@@ -38,9 +38,9 @@ const Dima = () => {
                 But she is in good hands with my sister. She gets plenty of
                 treats and outside time!
             </p>
-            <img className="dima-laying-sideways" src="dima-laying-sideways.jpg" alt="dima laying sideways"/>
-            <img className="dima-sunlight" src="dima-sunlight.jpg" alt="Dima laying in the sunlight"/>
-            <img className="dima-stare" src="dima-stare.jpg" alt="dima staring at the camera"/>
+            <img className="dima-laying-sideways" src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/dima-laying-sideways.jpg?raw=true" alt="dima laying sideways"/>
+            <img className="dima-sunlight" src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/dima-sunlight.jpg?raw=true" alt="Dima laying in the sunlight"/>
+            <img className="dima-stare" src="https://github.com/EyesWithoutFace/Portfolio-website/blob/main/react-app/build/dima-stare.jpg?raw=true" alt="dima staring at the camera"/>
         </div>
         </>
     )
